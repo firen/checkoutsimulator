b6f049effaed222c05a131478ae2a44a5668e7e5
firen <firen2k5@gmail.com>
2016-05-06 20:19:06 +0200

    init commit

---
+++ src/main/java/eu/firen/CheckoutsimulatorApplication.java
@@ -0,0 +1,12 @@
+package eu.firen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutsimulatorApplication.class, args);
+	}
+}
